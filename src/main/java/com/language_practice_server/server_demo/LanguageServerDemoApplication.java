8f8d5d7fb8ff0ee0f9896e100304cd0a43d4caa7
zaitsev-serhei <zaytsevsergey2013@gmail.com>
2025-10-14 19:19:07 +0300

    added shared enums fro the project

--- src/main/java/com/language_practice_server/server_demo/LanguageServerDemoApplication.java
+++ src/main/java/com/language_practice_server/server_demo/LanguageServerDemoApplication.java
@@ -8,7 +8,7 @@ public class LanguageServerDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(LanguageServerDemoApplication.class, args);
-		System.out.println("I`m here");
+
 	}
 
 }
---
+++ src/main/java/com/language_practice_server/server_demo/domain/enums/TaskType.java
@@ -0,0 +1,8 @@
+package com.language_practice_server.server_demo.domain.enums;
+
+public enum TaskType {
+    TEST,
+    TRANSLATION,
+    READING,
+    VOCAB
+}
